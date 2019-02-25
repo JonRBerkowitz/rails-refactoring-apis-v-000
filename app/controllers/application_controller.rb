3efceadc5eace5bb6a9bc8e51f5b1385dfa0941f
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-25 21:44:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user
-
+  # lab already solved?
   private
 
     def authenticate_user
